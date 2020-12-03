c0185b1856b22f5ae8aefc9a80c25e68faa6237d
Koen van Gilst <koen@koenvangilst.nl>
2020-12-03 10:13:37 +0100

    upgrade cra

--- src/App.js
+++ src/App.js
@@ -16,105 +16,100 @@ Supports:
 
 */
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
 
-import Overlay from "./Overlay";
-import Button from "./Button";
-import Clock from "./Clock";
-import styles from "./App.module.css";
-
-const INIT = generateState();
-
-function generateState() {
-    return {
-        expectedHours: Math.round(Math.random() * 12 + 1), // hours [0 - 12]
-        expectedMinutes: (Math.round(Math.random() * 4 + 1) * 15) % 60, // minutes [0 - 59]
-        hoursHand: Math.round(Math.random() * 360), // in degrees
-        minutesHand: Math.round(Math.random() * 360), // in degrees
-        secondsHand: getSecondsInDegrees(),
-        tracking: "none",
-    };
-}
-
-function App() {
-    const [expectedHours, setExpectedHours] = useState(INIT.expectedHours);
-    const [expectedMinutes, setExpectedMinutes] = useState(
-        INIT.expectedMinutes
-    );
-    const [hoursHand, setHoursHand] = useState(INIT.hoursHand);
-    const [minutesHand, setMinutesHand] = useState(INIT.minutesHand);
-    const [secondsHand, setSecondsHand] = useState(INIT.secondsHand);
-    const [tracking, setTracking] = useState(INIT.tracking);
-
-    useEffect(() => {
-        setInterval(() => {
-            setSecondsHand(getSecondsInDegrees());
-        }, 1000);
-    }, []);
-
-    const resetState = () => {
-        const newState = generateState();
-        setExpectedHours(newState.expectedHours);
-        setExpectedMinutes(newState.expectedMinutes);
-        setHoursHand(newState.hoursHand);
-        setMinutesHand(newState.minutesHand);
-        setSecondsHand(newState.secondsHand);
-        setTracking(newState.tracking);
-    };
-
-    const isDone = () => {
-        // if user is still dragging hands, she's not done yet
-        if (tracking !== "none") return false;
-
-        // 12 'o clock = 360 = 0 degrees
-        const expectedHoursInDegrees = (expectedHours * 30) % 360;
-        const expectedMinutesInDegrees = expectedMinutes * 6;
-
-        return (
-            expectedHoursInDegrees === hoursHand &&
-            expectedMinutesInDegrees === minutesHand
-        );
-    };
-
-    return (
-        <div className={styles["background"]}>
-            {isDone() && (
-                <Overlay>
-                    <Button onClick={resetState}>Well done!</Button>
-                </Overlay>
-            )}
-            <div className={styles["app-content"]}>
-                <div className={styles["question"]}>
-                    <p>
-                        set the time to{" "}
-                        <b>
-                            {timeToDigitalStr(expectedHours, expectedMinutes)}
-                        </b>
-                    </p>
-                </div>
-                <Clock
-                    setTracking={setTracking}
-                    setHoursHand={setHoursHand}
-                    setMinutesHand={setMinutesHand}
-                    hoursHand={hoursHand}
-                    minutesHand={minutesHand}
-                    secondsHand={secondsHand}
-                    tracking={tracking}
-                />
-            </div>
-        </div>
-    );
-}
+import Overlay from './Overlay'
+import Button from './Button'
+import Clock from './Clock'
+import styles from './App.module.css'
 
 function getSecondsInDegrees() {
-    const now = new Date();
-    const seconds = now.getSeconds();
-    return seconds * 6;
+  const now = new Date()
+  const seconds = now.getSeconds()
+  return seconds * 6
 }
 
 function timeToDigitalStr(hours, minutes) {
-    const minutesStr = `${minutes}`.padStart(2, "0");
-    return `${hours}:${minutesStr}`;
+  const minutesStr = `${minutes}`.padStart(2, '0')
+  return `${hours}:${minutesStr}`
 }
 
-export default App;
+function generateState() {
+  return {
+    expectedHours: Math.round(Math.random() * 12 + 1), // hours [0 - 12]
+    expectedMinutes: (Math.round(Math.random() * 4 + 1) * 15) % 60, // minutes [0 - 59]
+    hoursHand: Math.round(Math.random() * 360), // in degrees
+    minutesHand: Math.round(Math.random() * 360), // in degrees
+    secondsHand: getSecondsInDegrees(),
+    tracking: 'none',
+  }
+}
+
+const INIT = generateState()
+
+function App() {
+  const [expectedHours, setExpectedHours] = useState(INIT.expectedHours)
+  const [expectedMinutes, setExpectedMinutes] = useState(INIT.expectedMinutes)
+  const [hoursHand, setHoursHand] = useState(INIT.hoursHand)
+  const [minutesHand, setMinutesHand] = useState(INIT.minutesHand)
+  const [secondsHand, setSecondsHand] = useState(INIT.secondsHand)
+  const [tracking, setTracking] = useState(INIT.tracking)
+
+  useEffect(() => {
+    setInterval(() => {
+      setSecondsHand(getSecondsInDegrees())
+    }, 1000)
+  }, [])
+
+  const resetState = () => {
+    const newState = generateState()
+    setExpectedHours(newState.expectedHours)
+    setExpectedMinutes(newState.expectedMinutes)
+    setHoursHand(newState.hoursHand)
+    setMinutesHand(newState.minutesHand)
+    setSecondsHand(newState.secondsHand)
+    setTracking(newState.tracking)
+  }
+
+  const isDone = () => {
+    // if user is still dragging hands, she's not done yet
+    if (tracking !== 'none') return false
+
+    // 12 'o clock = 360 = 0 degrees
+    const expectedHoursInDegrees = (expectedHours * 30) % 360
+    const expectedMinutesInDegrees = expectedMinutes * 6
+
+    return (
+      expectedHoursInDegrees === hoursHand &&
+      expectedMinutesInDegrees === minutesHand
+    )
+  }
+
+  return (
+    <div className={styles['background']}>
+      {isDone() && (
+        <Overlay>
+          <Button onClick={resetState}>Well done!</Button>
+        </Overlay>
+      )}
+      <div className={styles['app-content']}>
+        <div className={styles['question']}>
+          <p>
+            set the time to{' '}
+            <b>{timeToDigitalStr(expectedHours, expectedMinutes)}</b>
+          </p>
+        </div>
+        <Clock
+          setTracking={setTracking}
+          setHoursHand={setHoursHand}
+          setMinutesHand={setMinutesHand}
+          hoursHand={hoursHand}
+          minutesHand={minutesHand}
+          secondsHand={secondsHand}
+          tracking={tracking}
+        />
+      </div>
+    </div>
+  )
+}
+export default App
